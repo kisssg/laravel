2d33b0616362da8744198aedfc9687caf4d8249e
sucre.xu <sucre.xu@LCHS028.hcg.homecredit.net>
2020-07-08 14:51:47 +0800

    score data can be created manually, edit or delete

---
+++ resources/views/score/data/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app') 
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <a href='{{url("project")}}'>{{$project->name}}</a> -> Items 
+                    <span class="alert-success">{{old('msg')}}</span>
+                    <a class="btn btn-primary float-right" href="{{url('project/item/create?project='.$project->id)}}">New</a>
+                </div>
+                <div id="content" class="card-deck" style="margin:15px auto;">
+                    <table class="table table-responsive-lg">
+                        <thead>
+                        <th>Item</th><th>Order</th><th>Field</th><th>Type</th><th>Options/scores</th><th>Action</th>
+                        </thead>
+                        @foreach($project->items as $item)
+                        <tr>
+                            <td title='{{$item->sub_title}}'>{{$item->title}}</td>                           
+                            <td>{{$item->order}}</td>   
+                            <td>{{$item->score_field}}</td>
+                            <td>{{$item->option_type}}</td>   
+                            <td>{{$item->options}}<br/>
+                                {{$item->scores}}</td>   
+                            <td><a  href="{{url('project/item/'.$item->id."/edit")}}" class="btn btn-info btn-xs">Edit</a>
+                                <form action="{{ url('project/item/'.$item->id) }}" method="POST" onsubmit="return confirm('Really want to remove this item?');" style="display: inline;">
+                                    {{ method_field('DELETE') }}
+                                    {{ csrf_field() }}
+                                    <input type="submit" class="btn btn-danger btn-xs"  value="&times;"/>
+                                </form></td>
+                        </tr>
+                        @endforeach                    
+                    </table>
+                </div>
+                <div class='card-footer'></div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/score/project/show.blade.php
+++ resources/views/score/project/show.blade.php
@@ -1,32 +1,49 @@
 @extends('layouts.app') 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            {{$project->name}} data list {{$data->total()}}
-        </div>
-        <div id="content" class="card-body" >            
-            <score-data-search project_id="{{$project->id}}" query_string="{{\Request::get('s')}}"></score-data-search>
-            <table class='table table-striped  table-hover table-responsive-md'>
-                <thead>
-                    @foreach(explode(",",$project->data_list_column_alias) as $title)
-                <th>{{$title}}</th>                    
-                @endforeach
-                <th>Action</th>
-                </thead>
-                @foreach($data as $d)
-                <tr>
-                    @foreach(explode(",",$project->data_list_columns) as $title)
-                    <td>{{$d->$title}}</td>                    
-                    @endforeach
-                    <td>
-                <data-pick-button id='{{$d->id}}' user='{{$d->owner}}' project='{{$project->id}}' @set-data='setData' @set-score='setScore' ref="pickScoreBtn{{$d->id}}"></data-pick-button>
-                <audit-button id='{{$d->id}}' user='{{$d->owner}}' project='{{$project->id}}' @set-data='setData' @set-score='setScore' @set-audit='setAudit' ref="auditBtn{{$d->id}}">Audit</audit-button>
-            </td>
-                </tr>
-                @endforeach
-            </table>
-        </div>
-        <div class='card-footer'>{{$data->links()}}</div>
+<div class="card">
+    <div class="card-header text-center">
+        <div class="float-left">{{$project->name}} data list {{$data->total()}}</div>
+        <span class=" alert-success">{{old('msg')}}</span>
+        <div class="float-right"><a class="btn btn-sm btn-primary" href="{{url("project/data/create?project_id=".$project->id)}}">+</a></div>
     </div>
+    <div id="content" class="card-body" >            
+        <score-data-search project_id="{{$project->id}}" query_string="{{\Request::get('s')}}"></score-data-search>
+        <table class='table table-striped  table-hover table-responsive-md'>
+            <thead>
+                @foreach(explode(",",$project->data_list_column_alias) as $title)
+            <th>{{$title}}</th>                    
+            @endforeach
+            <th>Action</th>
+            </thead>
+            @foreach($data as $d)
+            <tr>
+                @foreach(explode(",",$project->data_list_columns) as $title)
+                <td>{{$d->$title}}</td>                    
+                @endforeach
+                <td>
+                    <span class="inline">
+                        <data-pick-button id='{{$d->id}}' user='{{$d->owner}}' project='{{$project->id}}' @set-data='setData' @set-score='setScore' ref="pickScoreBtn{{$d->id}}"></data-pick-button>
+                        <audit-button id='{{$d->id}}' user='{{$d->owner}}' project='{{$project->id}}' @set-data='setData' @set-score='setScore' @set-audit='setAudit' ref="auditBtn{{$d->id}}">Audit</audit-button>
+                        <div class="dropdown">
+                            <a class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                ...
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="{{url("project/data/".$d->id."/edit?project_id=".$project->id)}}">Edit</a>
+                                <form action="{{ url('project/data/'.$d->id.'?project_id='.$project->id) }}" method="POST" onsubmit="return confirm('Really want to remove this data?');">
+                                    {{ method_field('DELETE') }}
+                                    {{ csrf_field() }}
+                                    <input type="submit" class="dropdown-item"  value="Delete"/>
+                                </form>
+                            </div>
+                        </div>
+                    </span>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
+    <div class='card-footer'>{{$data->links()}}</div>
+</div>
 @include('score.project.modals')
 @endsection
