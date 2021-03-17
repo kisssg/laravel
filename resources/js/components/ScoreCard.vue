71664e6940a05f79e442b29a7b9de0be2aaf1493
sucre.xu <sucre.xu@LCHS028.hcg.homecredit.net>
2021-03-17 10:44:12 +0800

    scorecard appearance simplified

--- resources/js/components/ScoreCard.vue
+++ resources/js/components/ScoreCard.vue
@@ -1,141 +1,211 @@
 <template>
-    <div class="modal-content" style="background-color:#cce8cf">
-        <div class='modal-header'>Score Card            
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class='modal-body'>
-            <table class="table table-bordered table-hover">
-                <tbody>
-                <div v-for="(item,key,index) in data_to_score" :key="index" class="d-inline-block" style="padding:0 20px 3px 0;font-family: sans-serif;font-size: 16px;">
-                    <span style="border: 1px solid #515151;"><span style="background-color:#515151;color: white;align-content: center">
-                            <span style="font-size: 12px;padding:2px;">{{key}}</span>
-                        </span>
-                        <span class="user-select-all" style="font-size: 12px;padding:2px;">{{item}}</span>
-                    </span>        
-                </div>
-                <tr v-for="(item,key,index) in items" :key="index">
-                    <td v-if="item.option_type==='text'">
-                <text-score-item :item="item" :savedAnswer="score?score[item.score_field]:null" :ref="item.score_field"></text-score-item>
-                </td>    
-                <td v-else-if="item.option_type==='single'">
-                <single-select-score-item :item="item" :savedAnswer="score?score[item.score_field]:null" :ref="item.score_field"></single-select-score-item>
-                </td>
-                <td v-else>
-                Option type <b>{{item.option_type}}</b> not supported, please verify. 
-                </td>
-                </tr>
-                <tr>
-                    <td>
-                        <span style="font-family: Verdana;font-size: 18px; font-weight: bold;">Total Score:</span>
-                        <span class="float-right">{{totalScore}}</span>
-                    </td>
-                </tr>
-                <tr v-if="score">
-                    <td>
-                    <span>{{score.created_by+" created at "+score.created_at+". "}}</span>
-                    <span class="float-right" v-if="score.updated_by">{{score.updated_by+" updated at " + score.updated_at}}</span>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>     
-        <div class="modal-footer text-center">
-            <span class="float-right">{{msg}}</span>
-            <button type="button" class="btn btn-secondary float-left" data-dismiss="modal" aria-label="Close">
-                关闭
-            </button>
-            <input type="button" class="btn btn-primary" v-on:click="submitResult" value="保存"/>
-        </div>
+  <div class="modal-content" style="background-color: #cce8cf">
+    <div class="modal-header">
+      Score Card
+      <button
+        type="button"
+        class="close"
+        data-dismiss="modal"
+        aria-label="Close"
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
     </div>
+    <div class="modal-body">
+      <table class="table table-bordered table-hover">
+        <tbody>
+          <tr
+            v-for="(item, key, index) in data_to_score"
+            :key="index"
+            class="d-inline-block"
+            style="
+              padding: 0 20px 3px 0;
+              font-family: sans-serif;
+              font-size: 16px;
+            "
+          >
+            <span style="border: 1px solid #515151"
+              ><span
+                style="
+                  background-color: #515151;
+                  color: white;
+                  align-content: center;
+                "
+              >
+                <span style="font-size: 12px; padding: 2px">{{ key }}</span>
+              </span>
+              <span
+                class="user-select-all"
+                style="font-size: 12px; padding: 2px"
+                >{{ item }}</span
+              >
+            </span>
+          </tr>
+          <tr v-for="(item, key, index) in items" :key="index">
+            <td v-if="item.option_type === 'text'">
+              <text-score-item
+                :item="item"
+                :savedAnswer="score ? score[item.score_field] : null"
+                :ref="item.score_field"
+              ></text-score-item>
+            </td>
+            <td v-else-if="item.option_type === 'single'">
+              <single-select-score-item
+                :item="item"
+                :savedAnswer="score ? score[item.score_field] : null"
+                :ref="item.score_field"
+              ></single-select-score-item>
+            </td>
+            <td v-else>
+              Option type <b>{{ item.option_type }}</b> not supported, please
+              verify.
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span
+                style="font-family: Verdana; font-size: 18px; font-weight: bold"
+                >Total Score:</span
+              >
+              <span class="float-right">{{ totalScore }}</span>
+            </td>
+          </tr>
+          <tr v-if="score">
+            <td>
+              <span>{{
+                score.created_by + " created at " + score.created_at + ". "
+              }}</span>
+              <span class="float-right" v-if="score.updated_by">{{
+                score.updated_by + " updated at " + score.updated_at
+              }}</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="modal-footer text-center">
+      <span class="float-right">{{ msg }}</span>
+      <button
+        type="button"
+        class="btn btn-secondary float-left"
+        data-dismiss="modal"
+        aria-label="Close"
+      >
+        关闭
+      </button>
+      <input
+        type="button"
+        class="btn btn-primary"
+        v-on:click="submitResult"
+        value="保存"
+      />
+    </div>
+  </div>
 </template>
 <script>
-    export default{
-        props: ['project_id'],
-        mounted() {
-            console.log('score card mounted');
-            //console.log('data:');
-            //console.log(this.data_to_score);
-            this.getItems(this.project_id, this);
-            this.totalScore=this.$parent.score?this.$parent.score.score:null;
+export default {
+  props: ["project_id"],
+  mounted() {
+    console.log("score card mounted");
+    //console.log('data:');
+    //console.log(this.data_to_score);
+    this.getItems(this.project_id, this);
+    this.totalScore = this.$parent.score ? this.$parent.score.score : null;
+  },
+  data() {
+    return {
+      items: null,
+      totalScore: 0,
+      msg: "",
+    };
+  },
+  methods: {
+    getItems: _.debounce(function (id, vm) {
+      axios.get("item?json=1&project=" + id).then(
+        (res) => {
+          vm.items = res.data;
         },
-        data() {
-            return {
-                "items": null,
-                "totalScore": 0,
-                'msg': ''
-            };
-        },
-        methods: {
-            getItems: _.debounce(function (id, vm) {
-                axios.get('item?json=1&project=' + id).then(res => {
-                    vm.items = res.data;
-                }, error => {
-                    if (error.response.status !== 200) {
-                        console.log('error triggered');
-                    }
-                    return false;
-                });
-            }, 50),
-            submitResult() {
-                let score = {};
-                let scoreData = this.$refs,
-                        totalScore = 0,scoredCount=0,scoreNumerator=0,scoreDenominator=0;
-                for (let item in scoreData) {
-                    score[item] = scoreData[item][0].answer;
-                    if(score[item]!=='-' && scoreData[item][0].type==='single'){
-                        scoredCount++;
-                    }                    
-                    if(score[item] !=="-" && scoreData[item][0].needScore){
-                        scoreDenominator++;
-                        if(score[item] ==="1")
-                            scoreNumerator++;
-                    }
-                    if(scoreData[item][0].needScore)
-                        totalScore += scoreData[item][0].answer===null?0:Number(scoreData[item][0].score);
-                }
-                console.log('Denominator:'+scoreDenominator);
-                console.log('Numerator:'+scoreNumerator);
-                if(scoredCount<1){
-                    this.msg='At least 1 item must be scored.';
-                    return;
-                }
-                let totalScoreFixed = totalScore > 100 ? 100 : totalScore;//总分大于100分就是100分
-                totalScoreFixed = totalScore < 0 ? 0 : totalScore;//总分小于0分就是0分
-                let totalScoreFactor= scoreNumerator / scoreDenominator * 100; // denominator is count of items need to be scored and scorable, numerator is score goal.
-                totalScore=this.data_to_score.score_method==='1'?totalScoreFactor:totalScoreFixed;
-                console.log('fixed score:' + totalScoreFixed.toFixed(2)); // fixed score.
-                this.totalScore=isNaN(totalScore)?null:totalScore.toFixed(2);// if no items can be scored, score is null.
-                score.score=this.totalScore;
-                score.data_id = this.data_to_score.id;
-                score.project_id = this.project_id;
-                let url = "/project/score", args = score;
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                });
-                let parent = this.$parent, pickScoreBtn = 'pickScoreBtn' + score.data_id;
-                let vm = this;
-                vm.msg='Result submitting...';
-                $.post(url, args, function (data) {
-                    vm.msg = (data.msg);
-                    if (data.result === "success") {
-                        //@TODO:show the new score when changed.    
-                        parent.$refs[pickScoreBtn].score = data.score;
-                        $(".bs-score-card").modal('hide');
-                    }
-                }, 'json');
-            }
-        },
-        computed: {
-            data_to_score() {
-                return this.$parent.data_to_score;
-            },
-            score() {
-                return this.$parent.score;
-            }
+        (error) => {
+          if (error.response.status !== 200) {
+            console.log("error triggered");
+          }
+          return false;
         }
-    }
+      );
+    }, 50),
+    submitResult() {
+      let score = {};
+      let scoreData = this.$refs,
+        totalScore = 0,
+        scoredCount = 0,
+        scoreNumerator = 0,
+        scoreDenominator = 0;
+      for (let item in scoreData) {
+        score[item] = scoreData[item][0].answer;
+        if (score[item] !== "-" && scoreData[item][0].type === "single") {
+          scoredCount++;
+        }
+        if (score[item] !== "-" && scoreData[item][0].needScore) {
+          scoreDenominator++;
+          if (score[item] === "1") scoreNumerator++;
+        }
+        if (scoreData[item][0].needScore)
+          totalScore +=
+            scoreData[item][0].answer === null
+              ? 0
+              : Number(scoreData[item][0].score);
+      }
+      console.log("Denominator:" + scoreDenominator);
+      console.log("Numerator:" + scoreNumerator);
+      if (scoredCount < 1) {
+        this.msg = "At least 1 item must be scored.";
+        return;
+      }
+      let totalScoreFixed = totalScore > 100 ? 100 : totalScore; //总分大于100分就是100分
+      totalScoreFixed = totalScore < 0 ? 0 : totalScore; //总分小于0分就是0分
+      let totalScoreFactor = (scoreNumerator / scoreDenominator) * 100; // denominator is count of items need to be scored and scorable, numerator is score goal.
+      totalScore =
+        this.data_to_score.score_method === "1"
+          ? totalScoreFactor
+          : totalScoreFixed;
+      console.log("fixed score:" + totalScoreFixed.toFixed(2)); // fixed score.
+      this.totalScore = isNaN(totalScore) ? null : totalScore.toFixed(2); // if no items can be scored, score is null.
+      score.score = this.totalScore;
+      score.data_id = this.data_to_score.id;
+      score.project_id = this.project_id;
+      let url = "/project/score",
+        args = score;
+      $.ajaxSetup({
+        headers: {
+          "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+        },
+      });
+      let parent = this.$parent,
+        pickScoreBtn = "pickScoreBtn" + score.data_id;
+      let vm = this;
+      vm.msg = "Result submitting...";
+      $.post(
+        url,
+        args,
+        function (data) {
+          vm.msg = data.msg;
+          if (data.result === "success") {
+            //@TODO:show the new score when changed.
+            parent.$refs[pickScoreBtn].score = data.score;
+            $(".bs-score-card").modal("hide");
+          }
+        },
+        "json"
+      );
+    },
+  },
+  computed: {
+    data_to_score() {
+      return this.$parent.data_to_score;
+    },
+    score() {
+      return this.$parent.score;
+    },
+  },
+};
 </script>
--- resources/js/components/TextScoreItem.vue
+++ resources/js/components/TextScoreItem.vue
@@ -1,30 +1,37 @@
 <template>
-    <div>
-        <span style="font-family: Verdana;font-size: 18px; font-weight: normal;">{{item.order+'.'+item.title}}</span><br>
-        <span style="font-family: Verdana;font-size: 9px;" class="text-muted">{{item.sub_title}}</span>
-        <span class="float-right" style="width:100%">
-            <textarea class="form-control " v-model="answer">
-            </textarea>                  
-        </span>
-    </div>
+  <div>
+    <span style="font-family: Verdana; font-size: 18px; font-weight: normal">{{
+      item.order + "." + item.title
+    }}</span
+    ><br />
+    <span v-if="show_description" style="font-family: Verdana; font-size: 9px" class="text-muted">{{
+      item.sub_title
+    }}</span>
+    <span class="float-right" style="width: 100%">
+      <textarea class="form-control" v-model="answer"> </textarea>
+    </span>
+  </div>
 </template>
 <script>
-    export default{
-        props: ['item','savedAnswer'],
-        mounted() {
-            this.answer=this.savedAnswer;
-        },
-        data() {
-            return {
-                "score": null,
-                "answer":null,
-                "type":"text",
-                "needScore":false
-            };
-        },
-        methods: {
-        },
-        computed: {
-        }
-    }
+export default {
+  props: ["item", "savedAnswer"],
+  mounted() {
+    this.answer = this.savedAnswer;
+  },
+  data() {
+    return {
+      score: null,
+      answer: null,
+      type: "text",
+      needScore: false,
+    };
+  },
+  methods: {},
+  computed: {
+    show_description() {
+      var urlParams = new URLSearchParams(window.location.search);
+      return urlParams.has("show_description"); //
+    },
+  },
+};
 </script>
